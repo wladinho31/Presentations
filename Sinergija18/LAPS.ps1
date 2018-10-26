e6a27e4d0f41bedf9585858e37df9f5ec7f6b70e
Vladimir Stefanović <38440074+Wladinho@users.noreply.github.com>
2018-10-26 10:57:59 +0200

    Add files via upload

---
+++ Sinergija18/LAPS.ps1
@@ -0,0 +1,17 @@
+﻿# Import LAPS PowerShell module and update schema
+Import-Module AdmPwd.PS
+Update-AdmPwdADSchema
+
+# Set OU of computers which needed LAPS
+Set-AdmPwdComputerSelfPermission -Identity Servers
+
+# Set AD group which has read permissions
+Set-AdmPwdReadPasswordPermission -OrgUnit Servers -AllowedPrincipals LAPSAdmins
+
+# Set AD group which has reset password permissions
+Set-AdmPwdResetPasswordPermission -OrgUnit Servers -AllowedPrincipals LAPSAdmins
+
+# Check local admin password
+Get-AdmPwdPassword -ComputerName SRV-01
+Get-AdmPwdPassword -ComputerName SRV-01 | Out-GridView
+
