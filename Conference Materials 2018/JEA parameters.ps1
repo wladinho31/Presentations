dfd11e76af6211d2d1d1404029d65cd5b729c458
Vladimir Stefanović <stefanovic.vladimir@hotmail.com>
2019-04-09 08:45:14 +0200

    Update

---
+++ Conference Materials 2018/JEA parameters.ps1
@@ -0,0 +1,13 @@
+﻿VisibleCmdlets = 'Restart-Computer', 'Get-NetIPAddress'
+
+VisibleCmdlets = @{ Name = 'Restart-Computer'; Parameters = @{ Name = 'Name' }}
+
+VisibleCmdlets = @{ Name = 'Restart-Service'; Parameters = @{ Name = 'Name'; ValidateSet = 'Dns', 'Spooler' }},
+                 @{ Name = 'Start-Website'; Parameters = @{ Name = 'Name'; ValidatePattern = 'HR_*' }}
+
+VisibleExternalCommands = 'C:\Windows\System32\whoami.exe'
+
+
+SessionType = ‘RestrictedRemoteServer’
+
+RoleDefinitions = @{ ‘Tech-Trainer\Help Desk’ = @{ RoleCapabilities = ‘HelpDeskJEA’ };}
